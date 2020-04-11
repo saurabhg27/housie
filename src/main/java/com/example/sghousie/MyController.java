b0e923f36a2dadf13a427776d8a021d6db117cc5
Saurabh <myemail>
2020-04-11 13:51:50 +0530

    bla

--- src/main/java/com/example/sghousie/MyController.java
+++ src/main/java/com/example/sghousie/MyController.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.type.TypeReference;
